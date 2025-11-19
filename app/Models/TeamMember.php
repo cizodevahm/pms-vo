92cc0cb8c44de6aaf6c9a9892d8c4a459dd561b2
luxprajapati <luxprajapati81@gmail.com>
2025-11-19 13:43:29 +0530

    Add Project, Task, and TeamMember models with relationships

---
+++ app/Models/TeamMember.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TeamMember extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'role',
+        'profile_photo'
+    ];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+}
